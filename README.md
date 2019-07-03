dee9287eb3ff564de6790e8b8f99cb49a967bc8d
cyberw0lf <dubey.abhineet@gmail.com>
2019-07-03 20:17:44 +0530

    added cli menu, added message when NASA APOD is not an image

--- README.md
+++ README.md
@@ -1,21 +1,17 @@
 ## OpenWall
 
 <p>
-A tiny python utility for updating openbox wallpaper from online sources at every login and or at set time
+A tiny python utility for updating openbox wallpaper from online sources at every login, at set time or manually
 </p>
-
-___
 
 ### Current Features:
 * Download Nasa APOD (Astronomy Picture of the Day)
 * Sets said picture as wallpaper
 * supports both feh and nitrogen to set wallpaper
 
-___
-
 ### TODO:
 * Add unsplash support
-* Add Flickr (Creative Commons) support
+* Add Flickr (Creative Commons) support. *doubtful*
 * Nasa APOD: check if already downloaded today's pic
 * Add support for scheduling wallpaper changes
 * Add config support
--- openwall.py
+++ openwall.py
@@ -11,7 +11,27 @@ parameters = {
     'hd': 'True'
 }
 
-def setWallpaper(wallpaper_file):
+def user_menu():
+    os.system('clear')
+    print("---------------------------------\n"
+            "       Welcome to OpenWall\n"
+            "---------------------------------\n\n"
+            "Enter the menu number to proceed\n\n"
+            "1. Download and set NASA APOD\n"
+            "2. Exit\n\n"
+            "---------------------------------\n\n")
+    while 1:
+        task=input(">> ")
+        if task=='1':
+            nasa_APOD()
+        elif task=='2':
+            print("Thank you for using OpenWall")
+            break
+        else:
+            print("woah there ! please stick to the menu item")
+
+
+def set_wallpaper(wallpaper_file):
     try:
         feh = subprocess.call(["feh", "--bg-fill", wallpaper_file])
     except OSError:
@@ -21,7 +41,7 @@ def setWallpaper(wallpaper_file):
     except OSError:
         pass
 
-def nasaAPOD():
+def nasa_APOD():
     timestamp = time.time()
     pictures_folder = os.path.join(os.getenv('HOME'),'Pictures')
     picture_name = 'openwall_' + str(timestamp) + '.jpg'
@@ -35,16 +55,18 @@ def nasaAPOD():
         if (data['media_type'] == "image"):
             response = requests.get(data['hdurl'])
             open(image_file, 'wb').write(response.content)
-            print ("Downloaded at location : " + pictures_folder)
-            setWallpaper(image_file)
-            print ("Wallpaper updated")
+            print("Downloaded at location : " + pictures_folder)
+            set_wallpaper(image_file)
+            print("Wallpaper updated")
+        else:
+            print("There doesn't seem to be an image today !")
 
     elif (response.status_code == 429):
-        print   ("Download error - You have made too many requests!")
+        print("Download error - You have made too many requests!")
 
     else:
-        print ("Download error - Could not download !")
-        print (response.status_code)
+        print("Download error - Could not download !")
+        print(response.status_code)
         return None
 
-nasaAPOD()
+user_menu()
